44f3befdbb4e690473935ee678b27d2f4bf9fbe0
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@63b2faa4-6ac5-df26-29d0-f68dd6c1a0eb>
2013-07-17 18:04:24 +0000

    Removing Old Content and unneeded folders

--- application/modules/report/controllers/IndexController.php
+++ application/modules/report/controllers/IndexController.php
@@ -16,4 +16,39 @@ class Report_IndexController extends Coda_Controller
         $job = Doctrine_Core::getTable('Coda_Model_Job')->findOneBy('jobId', $this->_request->getParam('jobId'));
         $this->view->job = $job;
     }
+
+    public function paidQuotesPdfAction()
+        {
+            $this->_helper->viewRenderer->setNoRender(true);
+            $this->_helper->layout->disableLayout(true);
+
+            $printLayout = new Zend_Layout();
+			$printLayout->setLayoutPath( APPLICATION_PATH . '/layouts/scripts' );
+			$printLayout->setLayout('print-bootstrap');
+
+            $quoteId = $this->getParam('id');
+
+            if( $quoteId ) {
+
+                //single
+                $printLayout->content .= $this->_generateQuotePdf($quoteId);
+            }
+            else {
+
+                //all
+                $paidQuotesQuery = LS_Model_ProposalTable::getPaidQuotes();
+
+                foreach ($paidQuotesQuery->execute() as $quote) {
+
+                     $printLayout->content .= $this->_generateQuotePdf($quote->id);
+                }
+            }
+
+            $pdfGen = new Xigen_Pdf( APPLICATION_PATH . '/bin/wkhtmltopdf' );
+            $pdfGen->setBody( $printLayout->render() );
+
+            $this->getResponse()
+                ->setHeader( 'Content-type', 'application/pdf' )
+                ->appendBody( $pdfGen->generate() );
+        }
 }
