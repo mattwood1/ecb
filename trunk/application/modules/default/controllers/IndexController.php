4f963d0d76507664213bed5b9c26291356c05800
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@63b2faa4-6ac5-df26-29d0-f68dd6c1a0eb>
2013-07-18 22:23:49 +0000

    Significant code improvements to the Dashboard.

--- trunk/application/modules/default/controllers/IndexController.php
+++ trunk/application/modules/default/controllers/IndexController.php
@@ -15,28 +15,18 @@ class IndexController extends Coda_Controller
 
     public function indexAction()
     {
-        // action body
-
         $wip = Doctrine_Query::create()->select('*')->from('Coda_Model_Job')->where('jobStatusId = ?', '4');
         $bookedIn = Doctrine_Query::create()->select('*')->from('Coda_Model_Job')->where('jobStatusId = ?', '3');
         $toBeBookedIn = Doctrine_Query::create()->select('*')->from('Coda_Model_Job')->where('jobStatusId = ?', '2');
         $rdyToInvoice = Doctrine_Query::create()->select('*')->from('Coda_Model_Job')->where('jobStatusId = ?', '5');
         $statuses = Doctrine_Core::getTable('Coda_Model_JobStatus')->findAll();
         $processes = Doctrine_Core::getTable('Coda_Model_JobProcess')->findAll();
-     // {  $diary = Doctrine_Core::getTable('Coda_Model_Diary')->create($form->getValues());
-       //     $diary->save();
-        //    $form->reset();}
-
 
         $this->view->wip = $wip->execute();
+        $this->view->rdyToInvoice = $rdyToInvoice->execute();
         $this->view->bookedIn = $bookedIn->execute();
         $this->view->toBeBookedIn = $toBeBookedIn->execute();
-        $this->view->rdyToInvoice = $rdyToInvoice->execute();
         $this->view->statuses = $statuses;
         $this->view->processes = $processes;
-
     }
-
-
 }
-
