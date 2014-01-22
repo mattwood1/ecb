1f3caa6fdde02dc426fec868e6418c0cc4b06c4f
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@63b2faa4-6ac5-df26-29d0-f68dd6c1a0eb>
2014-01-22 09:53:17 +0000

    Updated purchases SQL

--- application/scripts/purchases.sql
+++ application/scripts/purchases.sql
@@ -1,4 +1,4 @@
-CREATE  TABLE `ecb`.`purchase` (
+CREATE  TABLE `purchase` (
   `id` INT NOT NULL AUTO_INCREMENT ,
   `purchaseDate` DATE NULL ,
   `company` TEXT NULL ,
@@ -7,8 +7,8 @@ CREATE  TABLE `ecb`.`purchase` (
   `amount` DECIMAL(10,2) NULL ,
   `vat` DECIMAL(10,2) NULL ,
   `total` DECIMAL(10,2) NULL ,
-  `dueDate` DATE NULL 
-  `dateCreated` DATETIME NULL 
+  `dueDate` DATE NULL ,
+  `dateCreated` DATETIME NULL ,
   `dateModified` DATETIME NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = MyISAM
