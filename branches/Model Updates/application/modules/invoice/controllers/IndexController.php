92b360008cf1c811f231c0d8b3ff24368b2214b0
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@63b2faa4-6ac5-df26-29d0-f68dd6c1a0eb>
2014-01-24 23:25:00 +0000

    Added Invoice module

---
+++ branches/Model Updates/application/modules/invoice/controllers/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+class Invoice_IndexController extends Coda_Controller
+{
+    public function indexAction()
+    {
+
+    }
+
+    public function viewAction()
+    {
+
+    }
+
+    public function editAction()
+    {
+
+    }
+
+    public function addAction()
+    {
+
+    }
+}
