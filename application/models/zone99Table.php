6530622202969821f6c4e1d5169688aa2845680e
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@63b2faa4-6ac5-df26-29d0-f68dd6c1a0eb>
2014-02-03 10:30:18 +0000

    removing Z99 stuff

--- application/models/zone99Table.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ECB_Model_Zone99Table extends Doctrine_Record
-{
-    public static function getInstance()
-    {
-        return Doctrine_Core::getTable('ECB_Model_Zone99');
-    }
-
-}
