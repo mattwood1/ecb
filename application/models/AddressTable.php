5c29001962d11ba864872513d1a1134385274f93
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@63b2faa4-6ac5-df26-29d0-f68dd6c1a0eb>
2014-02-04 00:51:31 +0000

    Update issue 33

--- application/models/AddressTable.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ECB_Model_AddressTable extends Doctrine_Record
-{
-    public static function getInstance()
-    {
-        return Doctrine_Core::getTable('ECB_Model_Address');
-    }
-
-}
--- application/modules/address/Bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Address_Bootstrap extends Zend_Application_Module_Bootstrap
-{
-    
-}
