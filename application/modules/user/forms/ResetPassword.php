869dbf4e184ca1d28baac5465c57d9191e9902fa
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@63b2faa4-6ac5-df26-29d0-f68dd6c1a0eb>
2014-02-04 01:19:08 +0000

    Update issue 35

--- application/modules/user/forms/ResetPassword.php
+++ application/modules/user/forms/ResetPassword.php
@@ -1,22 +1,22 @@
 <?php
-class Auth_Form_ResetPassword extends Xigen_Form
+class Auth_Form_ResetPassword extends Twitter_Bootstrap_Form_Horizontal
 {
     public function setup()
-    {   
+    {
         $this->addElement('password', 'password', array(
             'label' => 'Password',
             'required' => true,
             'filters' => array('StripTags', 'StringTrim'),
             'validators' => array('Alnum')
         ));
-        
+
         $this->addElement('password', 'password2', array(
             'label' => 'Re-type Password',
             'required' => true,
             'filters' => array('StripTags', 'StringTrim'),
             'validators' => array('Alnum', array('identical', false, array('token' => 'password')))
         ));
-                
+
         $this->addSubmit('OK');
     }
 }
