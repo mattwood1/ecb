8b4be878569de1efb34b671fd8cafeb388daa473
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@63b2faa4-6ac5-df26-29d0-f68dd6c1a0eb>
2014-02-04 00:42:44 +0000

    Updates issue 32

--- branches/Release2/application/modules/board/controllers/IndexController.php
+++ branches/Release2/application/modules/board/controllers/IndexController.php
@@ -4,44 +4,42 @@ class Board_IndexController extends Zend_Controller_Action
 {
     public function indexAction()
     {
-        $wip = Doctrine_Query::create()->select('*')
-                                            ->from('ECB_Model_Job')
-                                            ->where('jobStatusId = ?', '4');
-
         $strip = Doctrine_Query::create()->select('*')
                                             ->from('ECB_Model_Job')
-                                            ->where('jobStatusId = 4 AND jobProcessId = ?', '1'); // Repair ?
+                                            ->where('jobStatusId = 4 AND jobProcessId = ?', '1'); // Strip
 
-        $fit = Doctrine_Query::create()->select('*')
+        $polishFit = Doctrine_Query::create()->select('*')
                                             ->from('ECB_Model_Job')
-                                            ->where('jobStatusId = 4 AND jobProcessId = ?', '2'); // Repair ?
+                                            ->where('jobStatusId = 4 AND jobProcessId = ?', '2') // Polish
+                                            ->orWhere('jobStatusId = ?', '5');                   // Fit
 
         $paint = Doctrine_Query::create()->select('*')
                                             ->from('ECB_Model_Job')
-                                            ->where('jobStatusId = 4 AND jobProcessId = ?', '3'); // Prime ?
+                                            ->where('jobStatusId = 4 AND jobProcessId = ?', '3'); // Paint
 
         $repair = Doctrine_Query::create()->select('*')
                                             ->from('ECB_Model_Job')
-                                            ->where('jobStatusId = 4 AND jobProcessId = ?', '4'); // Paint ?
+                                            ->where('jobStatusId = 4 AND jobProcessId = ?', '4'); // Repair
 
         $prime = Doctrine_Query::create()->select('*')
                                             ->from('ECB_Model_Job')
-                                            ->where('jobStatusId = 4 AND jobProcessId = ?', '5'); // Fit ?
+                                            ->where('jobStatusId = 4 AND jobProcessId = ?', '5'); // Prime
 
-        $polishWash = Doctrine_Query::create()->select('*')
+        $wash = Doctrine_Query::create()->select('*')
                                             ->from('ECB_Model_Job')
-                                            ->where('jobStatusId = 4 AND (jobProcessId = 5 OR jobProcessId = 6)'); // Wash ?
+                                            ->where('jobStatusId = 4 AND (jobProcessId = 5 OR jobProcessId = 6)'); // Wash
 
         $statuses = Doctrine_Core::getTable('ECB_Model_JobStatus')->findAll();
+
         $processes = Doctrine_Core::getTable('ECB_Model_JobProcess')->findAll();
 
         //$this->view->wip = $wip->execute();
         $this->view->strip      = $strip->execute();
-        $this->view->fit        = $fit->execute();
+        $this->view->polishFit  = $polishFit->execute();
         $this->view->paint      = $paint->execute();
         $this->view->repair     = $repair->execute();
         $this->view->prime      = $prime->execute();
-        $this->view->polishWash = $polishWash->execute();
+        $this->view->wash       = $wash->execute();
         $this->view->processes  = $processes;
     }
 
