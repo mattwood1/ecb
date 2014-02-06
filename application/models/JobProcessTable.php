f8c60aacfbc71b7ee9d0d20e14d8310f1b5d6a71
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@63b2faa4-6ac5-df26-29d0-f68dd6c1a0eb>
2014-02-06 23:28:15 +0000

    Update issue 37

--- application/models/JobProcessTable.php
+++ application/models/JobProcessTable.php
@@ -6,4 +6,19 @@ class ECB_Model_JobProcessTable extends Doctrine_Record
         return Doctrine_Core::getTable('ECB_Model_JobProcess');
     }
 
+    public function getPairs()
+    {
+        $query = $this->getInstance()
+            ->createQuery('jp')
+            ->select('jp.jobProcessId, jp.label');
+        $processes = $query->execute();
+
+        $array = array();
+        foreach ($processes as $process) {
+            $array[$process->jobProcessId] = $process->label;
+        }
+
+        return $array;
+    }
+
 }
--- application/models/JobStatusTable.php
+++ application/models/JobStatusTable.php
@@ -10,4 +10,19 @@ class ECB_Model_JobStatusTable extends Doctrine_Record
         return Doctrine_Core::getTable('ECB_Model_JobStatus');
     }
 
+    public function getPairs()
+    {
+        $query = $this->getInstance()
+            ->createQuery('js')
+            ->select('js.jobStatusId, js.label');
+        $statuses = $query->execute();
+
+        $array = array();
+        foreach ($statuses as $status) {
+            $array[$status->jobStatusId] = $status->label;
+        }
+
+        return $array;
+    }
+
 }
