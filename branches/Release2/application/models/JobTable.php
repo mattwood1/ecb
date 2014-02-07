08b40930b335aa9fa185f2b24c45dbeda8d96411
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@63b2faa4-6ac5-df26-29d0-f68dd6c1a0eb>
2014-02-07 01:57:55 +0000

    Update issue 36

--- branches/Release2/application/models/JobTable.php
+++ branches/Release2/application/models/JobTable.php
@@ -6,17 +6,30 @@ class ECB_Model_JobTable extends Doctrine_Record
         return Doctrine_Core::getTable('ECB_Model_Job');
     }
 
-    public function getJobs(array $status = array(), array $process = array())
+    public function getJobs(array $status = array(), array $process = array(), $keyword = null)
     {
         $query = $this->getInstance()->createQuery('j')
                 ->orderBy('CASE jobSource WHEN "TMI" THEN 1 ELSE 2 END, dateModified DESC');
 
-        if ($status) {
+        //filters
+        $status = array_filter($status);
+        $process = array_filter($process);
+
+        if (!empty($status)) {
             $query->whereIn('jobStatusId', $status);
         }
 
-        if ($process) {
+        if (!empty($process)) {
             $query->whereIn('jobProcessId', $process);
+        }
+
+        if ($keyword) {
+            $query->where('carReg LIKE ?', '%'.$keyword.'%')
+                ->orWhere('name LIKE ?', '%'.$keyword.'%')
+                ->orWhere('make LIKE ?', '%'.$keyword.'%')
+                ->orWhere('model LIKE ?', '%'.$keyword.'%')
+                ->orWhere('vin LIKE ?', '%'.$keyword.'%')
+                ->orWhere('postcode LIKE ?', '%'.$keyword.'%');
         }
 
         return $query->execute();
