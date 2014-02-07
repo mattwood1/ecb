e09de5b5217f1a9bea297d61357e393d3ee849e3
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@63b2faa4-6ac5-df26-29d0-f68dd6c1a0eb>
2014-02-07 01:32:32 +0000

    Update issue 32

--- branches/Release2/application/models/JobTable.php
+++ branches/Release2/application/models/JobTable.php
@@ -1,23 +1,9 @@
 <?php
 class ECB_Model_JobTable extends Doctrine_Record
 {
-    protected $_query;
-    protected $_order;
-
-    const ESTIMATE = 1;
-    const TO_BOOK_IN = 2;
-    const BOOKED_IN = 3;
-    const WIP = 4;
-    const COMPLETE = 5;
-
     public static function getInstance()
     {
         return Doctrine_Core::getTable('ECB_Model_Job');
-    }
-
-    public function getQuery()
-    {
-        return $this->_query;
     }
 
     public function getJobs(array $status, array $process = array())
@@ -29,32 +15,6 @@ class ECB_Model_JobTable extends Doctrine_Record
             $query->whereIn('jobProcessId', $process);
         }
         return $query->execute();
-    }
-
-    public function orderJobs($params)
-    {
-        $this->_query->orderBy($params);
-    }
-
-    public function searchJobs($keyword)
-    {
-        $this->_query
-            ->where('carReg LIKE ?', '%'.$keyword.'%')
-            ->orWhere('name LIKE ?', '%'.$keyword.'%')
-            ->orWhere('make LIKE ?', '%'.$keyword.'%')
-            ->orWhere('model LIKE ?', '%'.$keyword.'%')
-            ->orWhere('vin LIKE ?', '%'.$keyword.'%')
-            ->orWhere('postcode LIKE ?', '%'.$keyword.'%');
-    }
-
-    public function filterStatus($statusId)
-    {
-        $this->_query->where('jobStatusId = ?', $statusId);
-    }
-
-    public function filterProcess($processId)
-    {
-        $this->_query->where('jobProcessId = ?', $processId);
     }
 
 }
