a5cdc31528457198e2d708e5ae21962b6b3a6095
matthew.john.wood@gmail.com <matthew.john.wood@gmail.com@63b2faa4-6ac5-df26-29d0-f68dd6c1a0eb>
2014-02-12 00:33:32 +0000

    Update issue 35

--- branches/Release2/application/modules/user/controllers/IndexController.php
+++ branches/Release2/application/modules/user/controllers/IndexController.php
@@ -11,7 +11,20 @@ class User_IndexController extends Coda_Controller
 
     public function editAction()
     {
-        //$form =
+        $form = new User_Form_User();
+
+        $user = ECB_Model_UserTable::getInstance()->findOneBy('userId', $this->_request->getParam('id'));
+
+        if ($this->_request->isPost() && $form->isValid($this->_request->getPost())) {
+            $user->fromArray($form->getValues());
+            $user->save();
+            $this->_flash('User has been updated.', Coda_Helper_Flash::SUCCESS);
+            $this->gotoRoute(array('action' => 'index'));
+        }
+
+        $form->populate($user->toArray());
+
+        $this->view->form = $form;
     }
 
     public function deleteAction()
